44d2e48c51ccb3783585404aa04e70559278a3b3
r4b2 <github@r4b2.de>
2024-04-11 22:53:26 +0200

    feat: use client to fetch data

--- script.js
+++ script.js
@@ -20,58 +20,20 @@ function formatTime(seconds) {
 // Show loading animation when the page loads
 showLoading();
 
-// Fetch data with loading animation
-fetch("https://6zbvtyc4yf4p42nerdhovqsn7m0xdkmy.lambda-url.eu-central-1.on.aws/squad/stats")
+
+// https://6zbvtyc4yf4p42nerdhovqsn7m0xdkmy.lambda-url.eu-central-1.on.aws/squad/servers
+
+
+let urlsArray = []; // Initialize an empty array to store URLs
+
+fetch("https://6zbvtyc4yf4p42nerdhovqsn7m0xdkmy.lambda-url.eu-central-1.on.aws/squad/servers")
     .then((response) => response.json())
     .then((jsonData) => {
-        // Hide the loading animation when data is loaded
-        hideLoading();
-
-        jsonData.sort((a, b) => b.player_count - a.player_count);
-
-        const serverInfoContainer = document.getElementById("serverInfoContainer");
+        // console.log(jsonData);
 
         jsonData.forEach((data) => {
-            const div = document.createElement("div");
-            div.classList.add("serverInfo");
-
-            const roundTimeFormatted = formatTime(data.round_time);
-
-            div.innerHTML = `
-                <p><strong class="heading">${data.server_full_name}</strong></p>
-                <p><strong>Players:</strong> ${data.player_count} / ${data.max_players}</p>
-                <p><strong>Queue: </strong> ${data.queue_count}</p>
-                <p><strong>Current Layer:</strong> ${data.layer_name}</p>
-                <p><strong>Next Layer:</strong> ${data.layer_name_next}</p>
-                <p><strong>Game Mode:</strong> ${data.game_mode}</p>
-                <p><strong>Team:</strong> ${data.team_one} vs ${data.team_two}</p>
-                <p><strong>Round time:</strong> ${roundTimeFormatted}</p>
-            `;
-
-            if (data.layer_name.includes("VAT_") || data.layer_name.includes("JensensRange") || data.layer_name.includes("HT_")) {
-                map_name = data.layer_name.split("_")[0];
-                layer = data.layer_name.split("_")[1];
-                mode = "Training";
-            } else {
-                map_name = data.layer_name.split("_")[0];
-                mode = data.layer_name.split("_")[1];
-                layer = data.layer_name.split("_")[2];
-            }
-
-            squadmaps = `http://squadmaps.com/#${map_name}`;
-
-            if (map_name === "Tallil") {
-                map_name = "Tallil+Outskirts";
-            }
-
-            squadlanes = `https://squadlanes.com/#map=${map_name}&layer=${mode}+${layer}`;
-
-            if (data.game_mode !== "AAS" && data.game_mode !== "Seed") {
-                div.innerHTML += `<p><strong><a href="${squadlanes}" target="_blank">Squadlanes</strong></p>`;
-            }
-
-            div.innerHTML += `<p><strong><a href="${squadmaps}" target="_blank">Squadmaps</a></strong></p>`;
-            serverInfoContainer.appendChild(div);
+            urlsArray.push(data.Url); // Push each URL into the array
+            console.log(data.Url);
         });
     })
     .catch((error) => {
@@ -79,3 +41,70 @@ fetch("https://6zbvtyc4yf4p42nerdhovqsn7m0xdkmy.lambda-url.eu-central-1.on.aws/s
         // Hide the loading animation in case of an error
         hideLoading();
     });
+
+// wait for fetch to finish and iterate thgrough the array
+
+
+hideLoading();
+
+
+
+// Fetch data with loading animation
+// fetch("https://6zbvtyc4yf4p42nerdhovqsn7m0xdkmy.lambda-url.eu-central-1.on.aws/squad/stats")
+//     .then((response) => response.json())
+//     .then((jsonData) => {
+//         // Hide the loading animation when data is loaded
+//         hideLoading();
+
+//         jsonData.sort((a, b) => b.player_count - a.player_count);
+
+//         const serverInfoContainer = document.getElementById("serverInfoContainer");
+
+//         jsonData.forEach((data) => {
+//             const div = document.createElement("div");
+//             div.classList.add("serverInfo");
+
+//             const roundTimeFormatted = formatTime(data.round_time);
+
+//             div.innerHTML = `
+//                 <p><strong class="heading">${data.server_full_name}</strong></p>
+//                 <p><strong>Players:</strong> ${data.player_count} / ${data.max_players}</p>
+//                 <p><strong>Queue: </strong> ${data.queue_count}</p>
+//                 <p><strong>Current Layer:</strong> ${data.layer_name}</p>
+//                 <p><strong>Next Layer:</strong> ${data.layer_name_next}</p>
+//                 <p><strong>Game Mode:</strong> ${data.game_mode}</p>
+//                 <p><strong>Team:</strong> ${data.team_one} vs ${data.team_two}</p>
+//                 <p><strong>Round time:</strong> ${roundTimeFormatted}</p>
+//             `;
+
+//             if (data.layer_name.includes("VAT_") || data.layer_name.includes("JensensRange") || data.layer_name.includes("HT_")) {
+//                 map_name = data.layer_name.split("_")[0];
+//                 layer = data.layer_name.split("_")[1];
+//                 mode = "Training";
+//             } else {
+//                 map_name = data.layer_name.split("_")[0];
+//                 mode = data.layer_name.split("_")[1];
+//                 layer = data.layer_name.split("_")[2];
+//             }
+
+//             squadmaps = `http://squadmaps.com/#${map_name}`;
+
+//             if (map_name === "Tallil") {
+//                 map_name = "Tallil+Outskirts";
+//             }
+
+//             squadlanes = `https://squadlanes.com/#map=${map_name}&layer=${mode}+${layer}`;
+
+//             if (data.game_mode !== "AAS" && data.game_mode !== "Seed") {
+//                 div.innerHTML += `<p><strong><a href="${squadlanes}" target="_blank">Squadlanes</strong></p>`;
+//             }
+
+//             div.innerHTML += `<p><strong><a href="${squadmaps}" target="_blank">Squadmaps</a></strong></p>`;
+//             serverInfoContainer.appendChild(div);
+//         });
+//     })
+//     .catch((error) => {
+//         console.error("Error fetching data:", error);
+//         // Hide the loading animation in case of an error
+//         hideLoading();
+//     });
